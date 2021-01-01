2a83c87ee6ad6923b23d14932228d0eec08f939a
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2021-01-02 00:38:33 +0530

    Added 2 more solutions

--- practice/Trees/Kth Smallest Element in a BST.py
+++ practice/Trees/Kth Smallest Element in a BST.py
@@ -6,6 +6,58 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
+        '''
+        # Method 1 (Heap)
+        
+        # Time: O(N Log K) --> O(N)
+        # Space: O(N)
+        
+        def dfs(root):
+            nonlocal l
+            
+            if not root:
+                return
+            
+            dfs(root.left)
+            dfs(root.right)
+            l.append(root.val)
+            
+            return l
+        
+        l = list()
+        dfs(root)
+
+        heapq.heapify(l)
+        
+        return heapq.nsmallest(k, l)[-1]
+        '''
+        
+        # Method 2 (Inorder)
+        
+        # Time: O(N)
+        # Space: O(N)
+        
+        def inorder(root):
+            nonlocal l
+            if not root:
+                return 
+            
+            inorder(root.left)
+            l.append(root.val)
+            inorder(root.right)
+            
+            return
+        
+        l = list()
+        inorder(root)
+        
+        return l[k - 1]
+        
+        '''
+        
+        # Time: O(N Log N)
+        # Space: O(N)
+        
         def helper(root):
             if root:
                 q, ret = [root], list([root.val])
@@ -27,3 +79,4 @@ class Solution:
         l = helper(root)
         l.sort()
         return l[k - 1]
+        '''
